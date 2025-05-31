b10bb02d09326dfd323ba832906982d7c5603963
Deimian Vásquez <deimianvasquez@gmail.com>
2025-05-31 01:38:30 +0000

    add many to many and one yo many

--- src/models.py
+++ src/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy import String, Boolean, ForeignKey, DateTime, func, Text, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+from typing import List
 
 db = SQLAlchemy()
 
@@ -22,6 +24,7 @@ db = SQLAlchemy()
     Esta es una relación de uno a uno 
 """
 
+
 class Parent(db.Model):
     __tablename__ = "parent"
 
@@ -32,6 +35,13 @@ class Parent(db.Model):
     # la relación con el hijo (para poder raer cosas del hijo)
     children: Mapped["Child"] = relationship(back_populates="parent")
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lo que quiera": self.email
+        }
+
 
 class Child(db.Model):
     __tablename__ = "child"
@@ -40,5 +50,80 @@ class Child(db.Model):
     name: Mapped[String] = mapped_column(String(80), nullable=True)
     parent_id: Mapped[int] = mapped_column(ForeignKey("parent.id"))
 
-    # la relación con el padre (para poder raer cosas del padre)
-    parents: Mapped["Parent"] = relationship(back_populates="children")
+    # la relación con el padre (para poder rae-->r cosas del padre)
+    parent: Mapped["Parent"] = relationship(back_populates="children")
+
+
+"""
+    Este es el ejemplo de uno a muchos 
+"""
+
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(80), nullable=False, unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
+
+    # relationshiop del ORM
+    tareas: Mapped[List["Todos"]] = relationship(
+        back_populates="usuarios_buenos")
+
+
+class Todos(db.Model):
+    __tablename__ = "todos"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    label: Mapped[str] = mapped_column(Text, nullable=False)
+    is_done: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"),  nullable=False)
+
+    # relationshiop del ORM
+    usuarios_buenos: Mapped["User"] = relationship(back_populates="tareas")
+
+
+"""
+    Este es el ejemplo de una relación muchos a muchos 
+"""
+
+association_table = Table(
+    "association",
+    db.metadata,
+    Column("student_id", ForeignKey("student.id")),
+    Column("course_id", ForeignKey("course.id"))
+)
+
+
+class Student(db.Model):
+    __tablename__ = "student"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    couses: Mapped[List["Course"]] = relationship(
+        "Course", secondary=association_table,
+        back_populates="students")
+
+
+class Course(db.Model):
+    __tablename__ = "course"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    students: Mapped[List["Student"]] = relationship(
+        "Student",
+        secondary=association_table,
+        back_populates="couses"
+    )
